cdc3877163489a233f266b73180699d655ecedd7
vipin agrahari <vipinkagrahari@gmail.com>
2015-12-05 12:19:54 +0530

    Add OnClick handler for buttons and minor design changes

--- app/src/main/java/com/tantram/www/myappportfolio/MainActivity.java
+++ app/src/main/java/com/tantram/www/myappportfolio/MainActivity.java
@@ -1,15 +1,16 @@
 package com.tantram.www.myappportfolio;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +42,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void buttonClick(View v) {
+
+
+        CharSequence buttonText = ((Button) findViewById(v.getId())).getText();
+        Toast.makeText(MainActivity.this, "This Button will launch my " + buttonText + " App", Toast.LENGTH_SHORT).show();
     }
 }
